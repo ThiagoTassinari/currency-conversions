7d63c43ec55a1cb01a2bb610837b34a959357eda
ThiagoTassinari <thiagotassinari12@gmail.com>
2022-02-21 17:03:30 -0300

    API connection error handling

---
+++ app.js
@@ -0,0 +1,62 @@
+const currencyOneEl = document.querySelector('[data-js="currency-one"]');
+const currencyTwoEl = document.querySelector('[data-js="currency-two"]');
+const currenciesEl = document.querySelector('[data-js="currencies-container"]');
+
+const url = 'https://v6.exchangerate-api.com/v6/6990586d758bdae9890bab2f/latest/USD';
+
+const getErrorMessage = errorType => ({
+  "unsupported-code": "A moeda não existe em nosso banco de dados",
+  "malformed-request": "O endpoint do seu request precisa seguir a estrutura à seguir: https://v6.exchangerate-api.com/v6/6990586d758bdae9890bab2f/latest/USD",
+  "invalid-key": "Sua chave da API não é válida",
+  "inactive-account": "Seu endereço de e-mail não foi confirmado",
+  "quota-reached": "Sua conta atingiu o número de solicitações permitidas pelo seu plano",
+})[errorType] || 'Não foi possível obter as informações.';
+
+const fetchExchangeRate = async () => {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error('Sua conexão falhou. Não foi possível obter as informações.');
+    }
+
+    const exchangeRateData = await response.json();
+  
+    if (exchangeRateData.result === 'error') {
+      throw new Error(getErrorMessage(exchangeRateData['error-type']));
+    }
+
+    return exchangeRateData;
+
+  } catch (err) {
+    const div = document.createElement('div');
+    const button = document.createElement('button');
+
+    div.textContent = err.message;
+    div.classList.add('alert', 'alert-warning', 'alert-dismissible', 'fade', 'show');
+    div.setAttribute('role', 'alert');
+    button.classList.add('btn-close');
+    button.setAttribute('type', 'button');
+    button.setAttribute('aria-label', 'Close');
+
+    button.addEventListener('click', () => {
+      div.remove();
+    })
+
+    div.appendChild(button);
+    currenciesEl.insertAdjacentElement('afterend', div);
+  }
+}
+
+const init = async () => {
+  const exchangeRateData = await fetchExchangeRate();
+
+  console.log(Object.keys(exchangeRateData.conversion_rates));
+  
+  const option = `<option>oi</option>`
+  
+  currencyOneEl.innerHTML = option;
+  currencyTwoEl.innerHTML = option;
+}
+
+init();
